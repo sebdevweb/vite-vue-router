ab409b7cfad39f3bc6f185f8a054764ce2236011
sebdevweb <spansini@squad.fr>
2024-10-10 11:25:10 +0200

    [VITE-VUE-ROUTER] Lazy loading routes

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
+  </div>  
+</template>
+
+<style lang="css">
+  #nav .vue-school-active-link{
+    color: red;
+    text-decoration: underline;
+  }
+</style>
