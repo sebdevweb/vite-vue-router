e6277bb597d25638b8e8db625b107d833d99d1e9
sebdevweb <spansini@squad.fr>
2024-10-10 10:02:39 +0200

    [VITE-VUE-ROUTER] Import assets & fetch datas (1)

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,17 @@
 <template>
-  <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-  <br>
-  <router-link to="/">Home</router-link> |
-  <router-link to="/about">About</router-link> |
-  <a href="/about">A tag About</a> <!-- the <a> link by clicking, refresh the page = not good for UX-->
-  <router-view></router-view>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
+  </div>
+  <div class="container">
+    <router-view></router-view>
+  </div>
+
 </template>
 
 <script setup>
 
 </script>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
